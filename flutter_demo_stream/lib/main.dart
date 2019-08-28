ca370a0ba87dc5eea388d7c63d5cd31b3b80e34f
Trung Vu <wtrungvu@gmail.com>
2019-08-28 13:40:45 +0700

    Add Demo Flutter StreamBuilder
    + return null

--- flutter_demo_stream/lib/main.dart
+++ flutter_demo_stream/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -13,22 +15,76 @@ class MyApp extends StatelessWidget {
         ),
         home: Center(
           child: Scaffold(
-            appBar: AppBar(
-              title: Text("Flutter Demo Stream"),
-            ),
-            body: Container(
-              alignment: Alignment.center,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "Hello",
-                    style: TextStyle(fontSize: 50.0),
-                  )
-                ],
+              appBar: AppBar(
+                title: Text("Flutter Demo Stream"),
               ),
-            ),
-          ),
+              body: HomeScreen()),
         ));
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  StreamController<String> timmerStreamController = StreamController<String>();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Stream<String> streamData() {
+    return null;
+  }
+
+  void initStream() {
+    Stream.periodic(Duration(seconds: 1), (value) {
+      return value;
+    }).take(10).listen((value) {
+      timmerStreamController.sink.add(value.toString());
+      if (value == 9) {
+        timmerStreamController.close();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            StreamBuilder<String>(
+              stream: streamData(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text(
+                    snapshot.error.toString(),
+                    style: TextStyle(fontSize: 50.0),
+                  );
+                }
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                    return Text("Not connected to the Stream or null");
+                  case ConnectionState.waiting:
+                    return Text("Awaiting interaction");
+                  case ConnectionState.active:
+                    return Text("${snapshot.data}",
+                        style: TextStyle(fontSize: 50.0));
+                  case ConnectionState.done:
+                    return Text("Stream has finished");
+                  default:
+                    return Text("Emty!");
+                }
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
